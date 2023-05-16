2ca63cb5aa5f0145e65612c02fdabf86b7dd544e
Litesoulo <debian205410@gmail.com>
2023-05-16 06:42:30 +0500

    added bottom selection

--- lib/src/widgets/choose_club_btn.dart
+++ lib/src/widgets/choose_club_btn.dart
@@ -1,10 +1,53 @@
+import 'dart:js';
 
+import 'package:app/src/widgets/blur_filter.dart';
 import 'package:flutter/material.dart';
 
 class ChooseClubButton extends StatelessWidget {
   const ChooseClubButton({
     super.key,
   });
+
+  _chooseClubModalOpen(ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      backgroundColor: Colors.white12,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      builder: (BuildContext context) {
+        return Container(
+          decoration: BoxDecoration(
+            color: Colors.red.shade700,
+            borderRadius: BorderRadius.circular(50),
+          ),
+          width: double.infinity,
+          margin: EdgeInsets.all(10),
+          height: 300,
+          child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: <Widget>[
+              Container(
+                height: 50,
+                child: const Center(child: Text('Entry A')),
+              ),
+              Divider(),
+              Container(
+                height: 50,
+                child: const Center(child: Text('Entry B')),
+              ),
+              Divider(),
+              Container(
+                height: 50,
+                child: const Center(child: Text('Entry C')),
+              ),
+              Divider(),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,17 +58,17 @@ class ChooseClubButton extends StatelessWidget {
         height: 40,
         child: FloatingActionButton(
           backgroundColor: Colors.redAccent.shade700,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20)),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
           elevation: 2,
-          onPressed: () {},
+          onPressed: () => _chooseClubModalOpen(context),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Text(
                 'Club №1: "Disnay Land"',
-                style: TextStyle(
-                    color: Colors.white, fontWeight: FontWeight.w600),
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
               ),
               SizedBox(
                 width: 20,
