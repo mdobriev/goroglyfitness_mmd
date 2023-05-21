cd8f7580d091c12dbf1ef156bb72f3a478173aa2
mdobriev <mdobriev@gmail.com>
2023-05-21 21:34:14 +0400

    parse

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:app/src/app.dart';
 import 'package:flutter/material.dart';
+//import 'package:parse_server_sdk';
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final keyApplicationId = 'appId';
+  final keyClientKey = 'clientKey';
+  final keyParseServerUrl = 'http://192.168.10.234:1337/parse';
+
+  await Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, autoSendSessionId: true);
+
   runApp(const MyApp());
 }
+//void main() {
+ // runApp(const MyApp());
+//}
