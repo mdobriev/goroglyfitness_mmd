c000edffd0b4ed067d6ba1014cef0ccb39ad45f9
mdobriev <mdobriev@gmail.com>
2023-05-22 14:00:18 +0400

    parse

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,34 @@ import 'package:flutter/material.dart';
 //import 'package:parse_server_sdk';
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final keyApplicationId = 'appId';
-  final keyClientKey = 'clientKey';
-  final keyParseServerUrl = 'http://192.168.10.234:1337/parse';
 
-  await Parse().initialize(keyApplicationId, keyParseServerUrl,
-      clientKey: keyClientKey, autoSendSessionId: true);
+  //await Parse().initialize(keyApplicationId, keyParseServerUrl,
+   //   clientKey: keyClientKey, autoSendSessionId: true);
 
-  runApp(const MyApp());
-}
-//void main() {
- // runApp(const MyApp());
-//}
+
+  void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    final keyApplicationId = 'myappId';
+    final keyClientKey = 'DXmeuwzNjZWV';
+    final keyParseServerUrl = 'http://192.168.10.234/parse';
+
+    await Parse().initialize(keyApplicationId, keyParseServerUrl,
+        clientKey: keyClientKey, autoSendSessionId: true);
+
+    var firstObject = ParseObject('FirstClass')
+      ..set(
+          'message', 'Hey ! First message from Flutter. Parse is now connected');
+    await firstObject.save();
+
+    print('done');
+
+  }
+
+
+
+
+
+
+
+
+
